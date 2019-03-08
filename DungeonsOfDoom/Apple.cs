beed617163cce4e4e6845ced1a7c97ad32c1e8c3
HenrikCheng <henrikcheng@live.se>
2019-03-08 16:29:39 +0100

    fixat apple

--- DungeonsOfDoom/Apple.cs
+++ DungeonsOfDoom/Apple.cs
@@ -8,24 +8,11 @@ namespace DungeonsOfDoom
     {
         public Apple(string name, int price, int weight ) : base (name, price, weight)
         {
-<<<<<<< HEAD
-
-=======
->>>>>>> a419211fba7f217598608e2ea12e177942a5d499
         }
         public override void ModifyPlayer(Character character)
         {
             character.Health += 10;
-<<<<<<< HEAD
-=======
             character.Damage += 50;
-            
->>>>>>> a419211fba7f217598608e2ea12e177942a5d499
         }
-        public override void ModifyPlayer(Character character)
-        {
-            character.Health -= 10;
-        }
-
     }
 }
