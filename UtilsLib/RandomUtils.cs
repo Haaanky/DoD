b5a82124992942205a3e57c094b37b75022fcbc1
Henrik NF <Henriknorfri@gmail.com>
2019-03-11 13:05:25 +0100

    #4 Added XML comments etc

--- UtilsLib/RandomUtils.cs
+++ UtilsLib/RandomUtils.cs
@@ -6,12 +6,22 @@ namespace UtilsLib
 {
     public static class RandomUtils
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="minValue"></param>
+        /// <param name="maxValue"></param>
+        /// <returns></returns>
         public static int RandomGenerator(int minValue, int maxValue)
         {
             Random random = new Random();
             return random.Next(minValue, maxValue);
         }
-
+        /// <summary>
+        /// Generates a new random number between 0 and 100. 
+        /// </summary>
+        /// <param name="percentageValue">Value to check for</param>
+        /// <returns></returns>
         public static bool CheckPercentage(int percentageValue)
         {
             Random random = new Random();
